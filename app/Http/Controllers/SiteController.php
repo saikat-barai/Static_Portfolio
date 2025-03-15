4a01d0d503fe46b22e80b8d123499da7daf795f2
Saikat Barai <saikatbarai299@.com>
2025-03-15 23:30:36 +0600

    home page complete

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -6,5 +6,23 @@ use Illuminate\Http\Request;
 
 class SiteController extends Controller
 {
-    //
+    function homePage(){
+        $data = [
+            'name' => 'Saikat',
+            'profession' => 'Laravel Developer'
+        ];
+        return view('home', compact('data'));
+    }
+    function aboutPage(){
+        return view('about');
+    }
+    function servicesPage(){
+        return view('services');
+    }
+    function portfolioPage(){
+        return view('portfolio');
+    }
+    function contactPage(){
+        return view('contact');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\SiteController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [SiteController::class, 'homePage'])->name('home');
+Route::get('/about', [SiteController::class, 'aboutPage'])->name('about');
+Route::get('/services', [SiteController::class, 'servicesPage'])->name('services');
+Route::get('/portfolio', [SiteController::class, 'portfolioPage'])->name('portfolio');
+Route::get('/contact', [SiteController::class, 'contactPage'])->name('contact');
